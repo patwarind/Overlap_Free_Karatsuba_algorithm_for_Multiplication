c36aea449bf2691e799beff9f8bca6765fa730cd
Nitin D. Patwari <87736424+patwarind@users.noreply.github.com>
2022-04-05 13:39:47 +0530

    Add files via upload

---
+++ OKA_93bit/overlap_module_3bit.v
@@ -0,0 +1,57 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 20.02.2022 04:00:15
+// Design Name: 
+// Module Name: overlap_module_4bit
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module overlap_module_3bit(
+    B2_in1,
+    B2_in2,
+    B2_in3,
+    B2_in4,
+    B2_out
+    );
+
+
+parameter n = 4;
+    
+input [n-2:0] B2_in1;    
+input [n-2:0] B2_in2;    
+input [n-2:0] B2_in3;
+input [n-2:0] B2_in4;
+
+output [2*n-2:0] B2_out;
+
+
+assign B2_out[0] = B2_in1[0];
+
+assign B2_out[2] = B2_in1[1]^B2_in4[0];
+
+assign B2_out[1] = B2_in2[0]^B2_in3[0];
+
+assign B2_out[3] = B2_in2[1]^B2_in3[1];
+
+assign B2_out[5] = B2_in2[2]^B2_in3[2];
+
+assign B2_out[4] = B2_in1[2]^B2_in4[1];
+
+assign B2_out[6] = B2_in4[2];
+
+    
+endmodule
